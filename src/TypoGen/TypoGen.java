500cdf4e8fa6a0a35f202ae0290fd808a3bee19b
John Ho <hoj3@uw.edu>
2015-05-15 02:30:27 -0700

    Got started on typo generator.
    I have gotten started on working on the typo generator.

--- src/TypoGen/TypoGen.java
+++ src/TypoGen/TypoGen.java
@@ -1,5 +1,41 @@
 package TypoGen;
 
-public class TypoGen {
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.*;
+import java.io.*;
 
+public class TypoGen {
+	private URL typoFileURL;
+	private URI typoFileURI;
+	private Path typoFilePath;
+	private List<String> typoFile;
+	private Map<String, TypoSet> typoSets;
+	
+	public TypoGen() {
+		try {
+			typoFileURL = new URL("https://raw.githubusercontent.com/iwek/typos/master/typos.txt");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try {
+			typoFileURI = typoFileURL.toURI();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		typoFilePath = Paths.get(typoFileURI);
+		typoSets = new HashMap<String, TypoSet>();
+		
+		try {
+			typoFile = Files.readAllLines(typoFilePath, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
---
+++ src/TypoGen/TypoSet.java
@@ -0,0 +1,8 @@
+package TypoGen;
+
+import java.util.List;
+
+public class TypoSet {
+	private int typoCount;
+	private List<String> typos;
+}
