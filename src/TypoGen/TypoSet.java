edd45f91cdfc96d608da5eb5a98050410f2c5647
John Ho <hoj3@uw.edu>
2015-05-20 23:34:55 -0700

    Completed (1st ver. of) TypoSet.java
    I have finished coding (the first version of) TypoSet.java. Unless major
    bugs or performance issues are discovered, the work on TypoSet.java is
    complete.

--- src/TypoGen/TypoSet.java
+++ src/TypoGen/TypoSet.java
@@ -1,8 +1,21 @@
 package TypoGen;
 
-import java.util.List;
+import java.util.*;
 
 public class TypoSet {
-	private int typoCount;
 	private List<String> typos;
+	private Random rand;
+	
+	public TypoSet() {
+		typos = new ArrayList<String>();
+		rand = new Random();
+	}
+	
+	public void insertTypo(String typo) {
+		typos.add(typo);
+	}
+	
+	public String getTypo() {
+		return typos.get(rand.nextInt(typos.size()));
+	}
 }
