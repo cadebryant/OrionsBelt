547add64c576c1a8fdf1d6f1711708971db75d89
John Ho <hoj3@uw.edu>
2015-05-23 21:25:33 -0700

    Added an "on/off" switch to tokenization feature.
    Since the plain text dataset has already been lowercased and tokenized,
    I've added an "on/off" switch to TypoGen's lowercasing and tokenization
    feature.

--- src/TypoGen/TypoGen.java
+++ src/TypoGen/TypoGen.java
@@ -18,6 +18,7 @@ public class TypoGen {
 	private Map<String, TypoSet> typoSets;
 	private String comma;
 	private String typoLine;
+	private boolean tokenize;
 
 	public TypoGen() {
 		typoFIS = TypoGen.class.getResourceAsStream("typos1.txt");
@@ -26,6 +27,67 @@ public class TypoGen {
 		typoSets = new HashMap<String, TypoSet>();
 		comma = "[,]";
 		typoLine = "";
+		tokenize = false;
+
+		try {
+			typoISR = new InputStreamReader(typoFIS);
+			typoISR2 = new InputStreamReader(typos2);
+			typoBR = new BufferedReader(typoISR);
+			typoBR2 = new BufferedReader(typoISR2);
+
+			/* Read lines from typos1.txt.
+			 * Both typos1.txt and typos2.txt are based on data
+			 * from the following file:
+			 * https://raw.githubusercontent.com/iwek/typos/master/typos.txt
+			 */
+			while (typoBR.ready()) {
+				typoLine = typoBR.readLine();
+				typoData.add(typoLine);
+			}
+
+			// Read lines from typos2.txt.
+			while (typoBR2.ready()) {
+				typoLine = typoBR2.readLine();
+				typoData.add(typoLine);
+			}
+
+			typoFIS.close();
+			typos2.close();
+			typoISR.close();
+			typoISR2.close();
+			typoBR.close();
+			typoBR2.close();
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e2) {
+			e2.printStackTrace();
+		}
+
+		for (String typoPair: typoData) {
+			String[] splitPair = typoPair.split(comma);
+
+			if (splitPair.length == 2) {
+				if (typoSets.containsKey(splitPair[1])) {
+					TypoSet currentSet = typoSets.remove(splitPair[1]);
+					currentSet.insertTypo(splitPair[0]);
+					typoSets.put(splitPair[1], currentSet);
+				} else {
+					TypoSet currentSet = new TypoSet();
+					currentSet.insertTypo(splitPair[0]);
+					typoSets.put(splitPair[1], currentSet);
+				}
+			}
+		}
+	}
+
+	public TypoGen(boolean onOrOff) {
+		typoFIS = TypoGen.class.getResourceAsStream("typos1.txt");
+		typos2 = TypoGen.class.getResourceAsStream("typos2.txt");
+		typoData = new ArrayList<String>();
+		typoSets = new HashMap<String, TypoSet>();
+		comma = "[,]";
+		typoLine = "";
+		tokenize = onOrOff;
 
 		try {
 			typoISR = new InputStreamReader(typoFIS);
@@ -100,8 +162,14 @@ public class TypoGen {
 	public String insertTypos(String inputText) {
 		String spacebar = "[ ]+";
 		String outputText = "";
-		String tokenizedInput = tokenizeText(inputText);
-		String[] splitTokens = tokenizedInput.split(spacebar);
+		String[] splitTokens;
+
+		if (tokenize) {
+			String tokenizedInput = tokenizeText(inputText);
+			splitTokens = tokenizedInput.split(spacebar);
+		} else {
+			splitTokens = inputText.split(spacebar);
+		}
 
 		for (String token: splitTokens) {
 			if (typoSets.containsKey(token)) {
--- src/TypoGen/TypoGenTest.java
+++ src/TypoGen/TypoGenTest.java
@@ -8,9 +8,15 @@ public class TypoGenTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String testString = "Set in the offices of two psychoanalysts, and in a very bad French restaurant, BEYOND THERAPY is a love story about two very confused people.";
+		String testString2 = "set in the offices of two psychoanalysts , and in a very bad french restaurant , beyond therapy is a love story about two very confused people .";
 		TypoGen typoMaker = new TypoGen();
+		TypoGen typoMaker2 = new TypoGen(false);
+		TypoGen typoMaker3 = new TypoGen(true);
 		System.out.println(typoMaker.insertTypos(testString));
-		System.out.println(typoMaker.insertTypos(testString));
-		System.out.println(typoMaker.insertTypos(testString));
+		System.out.println(typoMaker2.insertTypos(testString));
+		System.out.println(typoMaker3.insertTypos(testString));
+		System.out.println(typoMaker.insertTypos(testString2));
+		System.out.println(typoMaker2.insertTypos(testString2));
+		System.out.println(typoMaker3.insertTypos(testString2));
 	}
 }
