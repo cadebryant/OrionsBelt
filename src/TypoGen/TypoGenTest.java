f66cc443b732e5e5f72a2708f848cda5d2347075
John Ho <hoj3@uw.edu>
2015-05-25 00:08:32 -0700

    Added typo generation metrics.
    I have added a feature to TypoGen that allows it to supply the number of
    words changed, both as raw counts and as percentages. I've also started
    work on ConvertText.

--- src/TypoGen/TypoGenTest.java
+++ src/TypoGen/TypoGenTest.java
@@ -1,7 +1,6 @@
 package TypoGen;
 
 public class TypoGenTest {
-
 	/**
 	 * @param args
 	 */
@@ -10,13 +9,16 @@ public class TypoGenTest {
 		String testString = "Set in the offices of two psychoanalysts, and in a very bad French restaurant, BEYOND THERAPY is a love story about two very confused people.";
 		String testString2 = "set in the offices of two psychoanalysts , and in a very bad french restaurant , beyond therapy is a love story about two very confused people .";
 		TypoGen typoMaker = new TypoGen();
-		TypoGen typoMaker2 = new TypoGen(false);
-		TypoGen typoMaker3 = new TypoGen(true);
+		TypoGen typoMaker2 = new TypoGen(true);
+		TypoGen typoMaker3 = new TypoGen(false);
 		System.out.println(typoMaker.insertTypos(testString));
-		System.out.println(typoMaker2.insertTypos(testString));
-		System.out.println(typoMaker3.insertTypos(testString));
 		System.out.println(typoMaker.insertTypos(testString2));
+		System.out.println(Integer.toString(typoMaker.typoCount()) + " words were changed, which is " + Double.toString(typoMaker.typoPercentage()) + "% of the words.\n");
+		System.out.println(typoMaker2.insertTypos(testString));
 		System.out.println(typoMaker2.insertTypos(testString2));
+		System.out.println(Integer.toString(typoMaker2.typoCount()) + " words were changed, which is " + Double.toString(typoMaker2.typoPercentage()) + "% of the words.\n");
+		System.out.println(typoMaker3.insertTypos(testString));
 		System.out.println(typoMaker3.insertTypos(testString2));
+		System.out.println(Integer.toString(typoMaker3.typoCount()) + " words were changed, which is " + Double.toString(typoMaker3.typoPercentage()) + "% of the words.");
 	}
 }
