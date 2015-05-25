1469e01cb9ea0b88856bae2ef9cd0dc3b1823581
Cade Bryant <cadehbryant@gmail.com>
2015-05-25 16:10:41 -0400

    More work on SA logic & adding call to fuzzy matcher

--- src/SentimentAnalysis/SentimentAnalysis.java
+++ src/SentimentAnalysis/SentimentAnalysis.java
@@ -5,24 +5,21 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import com.cybozu.labs.langdetect.Detector;
-import com.cybozu.labs.langdetect.DetectorFactory;
-import com.cybozu.labs.langdetect.LangDetectException;
+import FuzzyMatch.FuzzyMatch;
+import FuzzyMatch.MatchType;
 
 public class SentimentAnalysis {
-
-	// path to language profiles for classifier
-	private static String langProfileDirectory = "./src/profiles/";
+	
+	// declare our fuzzy matcher:
+	private static FuzzyMatch fuzzy;
 
 	// used to store positive and negative words for scoring
 	static List<String> posWords = new ArrayList<String>();
 	static List<String> negWords = new ArrayList<String>();
 
-	// keep some stats! [-1 / 0 / 1 / not english / foursquare / no text to
+	// keep some stats! [-1 / 0 / 1 / no text to
 	// classify]
 	static int[] stats = new int[6];
 
@@ -31,9 +28,9 @@ public class SentimentAnalysis {
 	 * @throws IOException
 	 * @throws LangDetectException
 	 */
-	public static void analyze(String inputText) throws IOException,
-			LangDetectException {
+	public static void analyze(String inputText, boolean useFuzzy, float matchThreshold) throws IOException {
 
+		fuzzy = new FuzzyMatch(MatchType.Levenshtein, matchThreshold);
 		long startTime = System.currentTimeMillis();
 
 		// source: www.cs.uic.edu/~liub/FBS/sentiment-analysis.html
@@ -58,36 +55,9 @@ public class SentimentAnalysis {
 		posReader.close();
 		
 		int score = 0;
-		
-		// prepare language classifier
-		DetectorFactory.loadProfile(langProfileDirectory);
-		// store different languages
-		Map<String, Integer> langHitList = new HashMap<String, Integer>();
-
-		// detect language, using http://code.google.com/p/language-detection/
-		// has 99% accuracy
-		Detector detector;
-
-		// we need a new instance every time unfortunately...
-		detector = DetectorFactory.create();
-		detector.append(inputText);
-		// classify language!
-		String detectedLanguage = detector.detect();
-
-		// if it is not english...
-		if (detectedLanguage.equals("en") == false) {
-			stats[3]++;
-			score = 0;
-		} else {
-			score = getSentimentScore(inputText);
-			// ++ index so we won't have -1 and stuff...
-			stats[score + 1]++;
-		}
-
-		Integer currentCount = langHitList.get(detectedLanguage);
-		// ...save the detected language for some stats
-		langHitList.put(detectedLanguage,
-				(currentCount == null) ? 1 : currentCount + 1);
+		score = getSentimentScore(inputText, useFuzzy);
+		// ++ index so we won't have -1 and stuff...
+		stats[score + 1]++;
 	}
 
 	/**
@@ -100,40 +70,46 @@ public class SentimentAnalysis {
 	 * @return score int: if < 0 then -1, if > 0 then 1 otherwise 0 - we don't
 	 *         care about the actual delta
 	 */
-	private static int getSentimentScore(String input) {
-		// normalize!
-		input = input.toLowerCase();
-		input = input.trim();
-		// remove all non alpha-numeric non whitespace chars
-		input = input.replaceAll("[^a-zA-Z0-9\\s]", "");
-
-		int negCounter = 0;
-		int posCounter = 0;
-
-		// so what we got?
-		String[] words = input.split(" ");
-
-		// check if the current word appears in our reference lists...
-		for (int i = 0; i < words.length; i++) {
-			if (posWords.contains(words[i])) {
-				posCounter++;
+	private static int getSentimentScore(String input, boolean useFuzzyMatching) {
+		// apply fuzzy matching:
+		// TODO: this is a hack for now; obviously comparing the input to itself will return true :-)
+		boolean isMatch =  fuzzy.Compare(input, input);
+		// don't go into SA logic if fuzzy match fails, 
+		// unless the user specifies to NOT use fuzzy matching:
+		if (isMatch || !useFuzzyMatching) {
+			// normalize!
+			input = input.toLowerCase();
+			input = input.trim();
+			// remove all non alpha-numeric non whitespace chars
+			input = input.replaceAll("[^a-zA-Z0-9\\s]", "");
+	
+			int negCounter = 0;
+			int posCounter = 0;
+	
+			// so what we got?
+			String[] words = input.split(" ");
+	
+			// check if the current word appears in our reference lists...
+			for (int i = 0; i < words.length; i++) {
+				if (posWords.contains(words[i])) {
+					posCounter++;
+				}
+				if (negWords.contains(words[i])) {
+					negCounter++;
+				}
 			}
-			if (negWords.contains(words[i])) {
-				negCounter++;
+	
+			// positive matches MINUS negative matches
+			int result = (posCounter - negCounter);
+	
+			// negative?
+			if (result < 0) {
+				return -1;
+				// or positive?
+			} else if (result > 0) {
+				return 1;
 			}
 		}
-
-		// positive matches MINUS negative matches
-		int result = (posCounter - negCounter);
-
-		// negative?
-		if (result < 0) {
-			return -1;
-			// or positive?
-		} else if (result > 0) {
-			return 1;
-		}
-
 		// neutral to the rescue!
 		return 0;
 	}
