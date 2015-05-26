2798b71c6b47887ea10e025be8de1eae6566b71f
Cade Bryant <cadehbryant@gmail.com>
2015-05-25 20:21:43 -0400

    Added tester

---
+++ src/SentimentAnalysis/Tester.java
@@ -0,0 +1,17 @@
+package SentimentAnalysis;
+
+public class Tester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+		System.out.println(SentimentAnalysis.analyze("bad bad bad good good good bad", false, 0));
+		} catch (Exception e) {
+			
+		}
+
+	}
+
+}
