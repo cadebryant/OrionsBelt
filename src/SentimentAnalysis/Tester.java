e7e6bbab994d5d1e00c5300a008266c4ef27ff07
Cade Bryant <cadehbryant@gmail.com>
2015-05-31 01:57:39 -0400

    Still trying to fix UDF bug

--- src/SentimentAnalysis/Tester.java
+++ src/SentimentAnalysis/Tester.java
@@ -1,13 +1,15 @@
 package SentimentAnalysis;
-
+import com.ibm.avatar.algebra.datamodel.Text;
+import com.ibm.avatar.algebra.util.lang.LangCode;
 public class Tester {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Text text = new Text("good bad ugly", LangCode.en);
 		try {
-		//System.out.println(SentimentAnalysis.analyze("good good good", false, (float) 0.8));
+			System.out.println(SentimentAnalysis.analyze(text, 1, (float) 0.8));
 		} catch (Exception e) {
 			
 		}
