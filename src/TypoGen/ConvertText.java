5dcd2d70f817351422608660441d0759cae9b19b
John Ho <hoj3@uw.edu>
2015-06-05 19:22:13 -0700

    Incorporated Kevin's suggestions into ConvertText.
    I've modified ConvertText to incorporate Kevin's suggestions.

--- src/TypoGen/ConvertText.java
+++ src/TypoGen/ConvertText.java
@@ -9,100 +9,101 @@ public class ConvertText {
 	/**
 	 * @param args
 	 * **COMMAND LINE ARGUMENTS FOR CONVERTTEXT**
-	 * *Use the typo rate as your first command line argument.
-	 * *Use dataset directories as your remaining command line arguments.
-	 * *For example: 100 C:\txt_sentoken\neg C:\txt_sentoken\pos
+	 * *Use dataset directories as your command line arguments.
+	 * *For example: C:\txt_sentoken\neg C:\txt_sentoken\pos
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TypoGen typoMaker = new TypoGen(Integer.parseInt(args[0]));
+		String[] typoRates = {"0", "10", "25", "50", "100"};
 
-		for (int i = 1; i < args.length; i++) {
-			Path dir = Paths.get(args[i]);
-			Path typoDir = Paths.get(args[i] + "/typo_" + args[0]);
-			Path UTF8Dir = Paths.get(args[i] + "/UTF8");
-			int typoSum = 0;
-			int docCount = 0;
-			double percentSum = 0.0;
-			String typoMetrics = "";
-			File metricsFile = new File(args[i] + "_typo_metrics_" + args[0] + ".txt");
+		for (String typoRate: typoRates) {
+			TypoGen typoMaker = new TypoGen(Integer.parseInt(typoRate));
 
-			try {
-				Files.createDirectories(typoDir);
-				Files.createDirectories(UTF8Dir);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			for (String arg: args) {
+				Path dir = Paths.get(arg);
+				Path typoDir = Paths.get(arg + "/typos." + typoRate);
+				int typoSum = 0;
+				int docCount = 0;
+				double percentSum = 0.0;
+				String typoMetrics = "";
+				File metricsFile = new File(arg + "_typo_metrics_" + typoRate + ".txt");
 
-			try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.{txt}")) {
-				for (Path entry: stream) {
-					List<String> entryLines = Files.readAllLines(entry, Charset.defaultCharset());
-					String typoText = "";
-					String textUTF8 = "";
-					File typoFile = new File(args[i] + "/typo_" + args[0] + "/typo_" + args[0] + '_' + entry.getFileName());
-					File UTF8File = new File(args[i] + "/UTF8/UTF8_" + entry.getFileName());
-
-					for (String entryLine: entryLines) {
-						String tokenizedLine = typoMaker.tokenizeText(entryLine);
-						typoText = typoText + typoMaker.insertTypos(entryLine) + '\n';
-						textUTF8 = textUTF8 + tokenizedLine.replaceAll("\\s+", " ") + '\n';
-					}
-
-					typoMetrics = typoMetrics + entry.getFileName() + ": " + Integer.toString(typoMaker.typoCount()) + " words were changed, which is " + Double.toString(typoMaker.typoPercentage()) + "% of the words.\n";
-					typoSum += typoMaker.typoCount();
-					percentSum += typoMaker.typoPercentage();
-					docCount++;
-					typoMaker.resetCounts();
-					byte[] typoFileBytes = null;
-					byte[] UTF8FileBytes = null;
-
-					try {
-						typoFileBytes = UnicodeUtils.convert(typoText.getBytes(Charset.defaultCharset()), "UTF-8");
-					} catch (Exception b1) {
-						// TODO Auto-generated catch block
-						b1.printStackTrace();
-					}
-
-					try {
-						UTF8FileBytes = UnicodeUtils.convert(textUTF8.getBytes(Charset.defaultCharset()), "UTF-8");
-					} catch (Exception b2) {
-						// TODO Auto-generated catch block
-						b2.printStackTrace();
-					}
-
-					try (FileOutputStream writer1 = new FileOutputStream(typoFile)) {
-						writer1.write(typoFileBytes);
-					} catch (IOException v) {
-						v.printStackTrace();
-					}
-
-					try (FileOutputStream writer2 = new FileOutputStream(UTF8File)) {
-						writer2.write(UTF8FileBytes);
-					} catch (IOException w) {
-						w.printStackTrace();
-					}
+				try {
+					Files.createDirectories(typoDir);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-			} catch (IOException x) {
-				x.printStackTrace();
-			}
 
-			double avgTypos = (double) typoSum / (double) docCount;
-			double avgPercent = percentSum / (double) docCount;
-			typoMetrics = "Per document average: " + Double.toString(avgTypos) + " words were changed, which is " + Double.toString(avgPercent) + "% of the words.\n---------\n" + typoMetrics;
-			byte[] typoMetricsBytes = null;
+				try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.{txt}")) {
+					for (Path entry: stream) {
+						List<String> entryLines = Files.readAllLines(entry, Charset.defaultCharset());
+						String typoText = "";
+						String typoText2 = "";
+						String[] pivotWords = {"and", "but", "although", "however", "then", "though", "yet"};
+						File typoFile = new File(arg + "/typos." + typoRate + '/' + entry.getFileName());
 
-			try {
-				typoMetricsBytes = UnicodeUtils.convert(typoMetrics.getBytes(Charset.defaultCharset()), "UTF-8");
-			} catch (Exception b3) {
-				// TODO Auto-generated catch block
-				b3.printStackTrace();
-			}
+						for (String entryLine: entryLines) {
+							String typoedLine = typoMaker.insertTypos(entryLine);
+							typoText = typoText + typoedLine.replaceAll("[ \t\r\n]+", " ") + ' ';
+						}
 
-			try (FileOutputStream writer3 = new FileOutputStream(metricsFile)) {
-				writer3.write(typoMetricsBytes);
-			} catch (IOException y) {
-				y.printStackTrace();
+						for (char tc: typoText.toCharArray()) {
+							if (Character.isLetterOrDigit(tc)) {
+								typoText2 = typoText2 + tc;
+							} else if (tc == ' ') {
+								typoText2 = typoText2 + tc;
+							} else {
+								typoText2 = typoText2 + '\n' + tc;
+							}
+						}
+
+						for (String pivotWord: pivotWords) {
+							String typoText3 = typoText2.replaceAll(pivotWord, '\n' + pivotWord);
+							typoText2 = typoText3;
+						}
+
+						typoMetrics = typoMetrics + entry.getFileName() + ": " + Integer.toString(typoMaker.typoCount()) + " words were changed, which is " + Double.toString(typoMaker.typoPercentage()) + "% of the words.\n";
+						typoSum += typoMaker.typoCount();
+						percentSum += typoMaker.typoPercentage();
+						docCount++;
+						typoMaker.resetCounts();
+						byte[] typoFileBytes = null;
+
+						try {
+							typoFileBytes = UnicodeUtils.convert(typoText2.getBytes(Charset.defaultCharset()), "UTF-8");
+						} catch (Exception b1) {
+							// TODO Auto-generated catch block
+							b1.printStackTrace();
+						}
+
+						try (FileOutputStream writer1 = new FileOutputStream(typoFile)) {
+							writer1.write(typoFileBytes);
+						} catch (IOException v) {
+							v.printStackTrace();
+						}
+					}
+				} catch (IOException x) {
+					x.printStackTrace();
+				}
+
+				double avgTypos = (double) typoSum / (double) docCount;
+				double avgPercent = percentSum / (double) docCount;
+				typoMetrics = "Per document average: " + Double.toString(avgTypos) + " words were changed, which is " + Double.toString(avgPercent) + "% of the words.\n---------\n" + typoMetrics;
+				byte[] typoMetricsBytes = null;
+
+				try {
+					typoMetricsBytes = UnicodeUtils.convert(typoMetrics.getBytes(Charset.defaultCharset()), "UTF-8");
+				} catch (Exception b2) {
+					// TODO Auto-generated catch block
+					b2.printStackTrace();
+				}
+
+				try (FileOutputStream writer2 = new FileOutputStream(metricsFile)) {
+					writer2.write(typoMetricsBytes);
+				} catch (IOException y) {
+					y.printStackTrace();
+				}
 			}
 		}
 	}
