c3c647ce19c40ff65e0f0c91f663fa19b23334d3
Kevin Wonus <sales@wonus.com>
2015-06-06 00:05:59 -0400

    Updated FuzzyMatcher

--- src/FuzzyMatch/FuzzyMatch.java
+++ src/FuzzyMatch/FuzzyMatch.java
@@ -2,27 +2,50 @@ package FuzzyMatch;
 
 import uk.ac.shef.wit.simmetrics.similaritymetrics.*;
 
-public class FuzzyMatch {
-	
-	private AbstractStringMetric Metric;
-	private float AcceptableDistance;
-	public FuzzyMatch(MatchType type, float threshhold)
-	{
-		switch (type)
-		{
-		case Levenshtein:		Metric = new Levenshtein();			break;
-		case CosineSimilarity:	Metric = new CosineSimilarity();	break;
-		case EuclideanDistance: Metric = new EuclideanDistance();	break;
-		case MongeElkan:		Metric = new MongeElkan();			break;
-		case JaroWinkler:		Metric = new JaroWinkler();			break;
-		default:				Metric = new Levenshtein();			break;
-		}
-		AcceptableDistance = threshhold;
-	}
-	public boolean Compare(String phrase1, String phrase2)
-	{
-		float similarity = Metric.getSimilarity(phrase1, phrase2);
-		
-		return (similarity >= 1 - AcceptableDistance);
-	}
+public class FuzzyMatch
+{	
+    private AbstractStringMetric Metric;
+    private float AcceptableDistance;
+    public FuzzyMatch(MatchType type, float threshhold)
+    {
+        switch (type)
+        {
+            case Levenshtein:	Metric = new Levenshtein();		break;
+            case CosineSimilarity:	Metric = new CosineSimilarity();	break;
+            case EuclideanDistance: Metric = new EuclideanDistance();	break;
+            case MongeElkan:		Metric = new MongeElkan();		break;
+            case JaroWinkler:	Metric = new JaroWinkler();		break;
+            default:		Metric = new Levenshtein();		break;
+        }
+        AcceptableDistance = threshhold;
+    }
+    public float Compare(String string1, String string2)
+    {
+        float similarity = Metric.getSimilarity(string1, string2);
+
+        if (similarity >= 1 - AcceptableDistance)
+            return similarity;
+
+        return (float) 0.0;
+    }
+    public float MatchesNot(String word)
+    {
+        return Compare(word, "not");
+    }
+    public float MatchInArray(String word, String[] array)
+    {
+        float similarity = (float) 0.0;
+        int len = array.length;
+        for (int i = 0; i < len; i++)
+        {
+            float check = Compare(word, array[i]);
+
+            if (check >= 1.0)
+                return (float) 1.0;
+
+            else if (check > similarity)
+                similarity = check;
+        }
+        return similarity;
+    }
 }
